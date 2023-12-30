ad1d5788829d6dcbf085e80565b3bfec25c2c44d
DreamHarakiri <derek.gaming744@gmail.com>
2023-12-30 20:35:12 +0300

    fix gallery

--- assets/js/pictures.js
+++ assets/js/pictures.js
@@ -1,23 +1,23 @@
 export const initialPictures = [
   {
-    url: "../../gallery/1.jpg",
+    url: "../gallery/1.jpg",
   },
   {
-    url: "../../gallery/2.jpg",
+    url: "../gallery/2.jpg",
   },
   {
-    url: "../../gallery/3.jpg",
+    url: "../gallery/3.jpg",
   },
   {
-    url: "../../gallery/4.jpg",
+    url: "../gallery/4.jpg",
   },
   {
-    url: "../../gallery/5.jpg",
+    url: "../gallery/5.jpg",
   },
   {
-    url: "../../gallery/6.jpg",
+    url: "../gallery/6.jpg",
   },
   {
-    url: "../../gallery/7.jpg",
+    url: "../gallery/7.jpg",
   },
 ];
