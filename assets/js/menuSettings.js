08b3a204b70908bc0809b2d7b341c789a29abea6
DreamHarakiri <derek.gaming744@gmail.com>
2023-12-31 17:43:28 +0300

    phone adaptive

--- assets/js/menuSettings.js
+++ assets/js/menuSettings.js
@@ -1,5 +1,7 @@
 const menuNavigation = document.querySelector(".NavSection");
 const menuButton = document.querySelector(".menuButton");
+const btnCall = document.querySelector(".btnContact__Nav");
+const btnMap = document.querySelector(".findMap");
 
 function useMenu() {
   if (menuButton.classList.toggle("show")) {
@@ -13,4 +15,13 @@ function useMenu() {
 
 menuButton.addEventListener("click", function () {
   useMenu();
+});
+
+btnCall.addEventListener("click", function () {
+  document.location.href = "tel:+74852682348";
+});
+
+btnMap.addEventListener("click", function () {
+  document.location.href =
+    "https://yandex.ru/maps/16/yaroslavl/house/ulitsa_kalinina_18/Z0AYfgJiTEECQFttfXl2c39qbQ==/?from=mapframe&ll=39.852474%2C57.572590&source=mapframe&utm_source=mapframe&z=16";
 });
